07580a8855ee8efe3d3e478b9dd3d6d0ed4d0404
Y01610 <quanxin@yondervision.com.cn>
2019-11-14 15:20:19 +0800

    Get方法测试通过

--- src/main/java/com/example/gateway/GatewayGet.java
+++ src/main/java/com/example/gateway/GatewayGet.java
@@ -16,7 +16,7 @@ public class GatewayGet {
     public static void main(String[] args) {
 
         Map<String, String> params = new HashMap<>();
-        params.put("name", "zhangsan");
+        params.put("name", "张三");
         params.put("code", "201801");
 
         SDKKey key = new SDKKey();
@@ -25,11 +25,12 @@ public class GatewayGet {
         key.setIv("UISwD9fW6cFh9SNS");
         key.setPrik("ce/2iZsuhncEfhZNp0N1aNBOvATN7DEWGNhNBYsGq3A=");
         key.setUserId("ALICE123@YAHOO.COM");
-        key.setNeedSecret(true); // 设置是否加密
+        key.setNeedSecret(false); // 返回结果是否加密
+        key.setAppId("app_id_sign");
 
         HttpClientUtil util = HttpClientUtil.getInstance();
         try {
-            Object ret = util.get("http://127.0.0.1:8000/sdk/rest/getTest", params, key);
+            Object ret = util.get("http://127.0.0.1:8000/gateway/04320/07/A00003/getTest", params, key);
             System.out.println(ret);
         } catch (Exception e) {
             e.printStackTrace();
