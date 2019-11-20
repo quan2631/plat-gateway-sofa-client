0ac99edbaac15cdcbb23848589b5cca0d5c503d5
Y01610 <quanxin@yondervision.com.cn>
2019-11-20 11:08:25 +0800

    将sofa服务单独封装为jar后的调用

--- src/main/java/com/example/gateway/SofaServiceController.java
+++ src/main/java/com/example/gateway/SofaServiceController.java
@@ -1,9 +1,7 @@
 package com.example.gateway;
 
-import com.wish.techmidplat.gateway.inter.ISofa;
-import com.wish.techmidplat.gateway.sdk.utils.SofaUtil;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import com.wish.techmidplat.gateway.api.GatewaySofaApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,27 +17,18 @@ import java.util.Map;
 @RestController
 public class SofaServiceController {
 
-    @Value("${com.with.gateway.server}")
-    private  String server;
-
-    @Value("${com.with.gateway.cityId}")
-    private  String cityId;
+    @Autowired
+    private GatewaySofaApi gatewaySofaApi;
 
     @RequestMapping("/action")
-    private  String action(){
-        ISofa gatewaySofaService = SofaUtil.getGatewaySofaService(server);
-        Object o = gatewaySofaService.actionStr(cityId,"2","3");
-        System.out.println(o); // you input cityCode is 1
-        return  o.toString();
+    private  String actionStr(){
+        return  gatewaySofaApi.actionStr("2","3");
     }
 
     @RequestMapping("/action1")
-    private  String action1(){
-        ISofa gatewaySofaService = SofaUtil.getGatewaySofaService(server);
+    private  String actionMap(){
         Map<String, Object> paramsMap = new HashMap<>();
         paramsMap.put("1", "111");
-        Object o = gatewaySofaService.actionMap(cityId,"2",paramsMap);
-        System.out.println(o); // {1=111, key=this is you input map.}
-        return  o.toString();
+        return  gatewaySofaApi.actionMap("2",paramsMap).toString();
     }
 }
